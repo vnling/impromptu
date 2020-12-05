1a1ac73f0781cb62b4d3dacc94d34027ef1b1d6e
Chris <pcc386@nyu.edu>
2020-12-05 22:12:57 +0400

    added styling and video page with buttons

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-let video = document.getElementById('video');
-video.addEventListener('ended', playNextVideo, false);
+let video = document.getElementById("video");
+let playButton = document.getElementById("play");
+let choiceDiv = document.getElementById("choiceDivID");
+let choice1 = document.getElementById("choice1");
+let choice2 = document.getElementById("choice2");
+video.addEventListener("ended", playNextVideo, false);
 
+playButton.onclick = function () {
+  video.play();
+  console.log("test");
+  playButton.style.display = "none";
+};
 function playNextVideo() {
-    //play the next video
+  choiceDiv.style.display = "flex";
 }
+choice1.onclick = function () {
+  console.log("psyduck");
+  video.setAttribute("src", "psyduck.mp4");
+  video.play();
+  choiceDiv.style.display = "none";
+};
+choice2.onclick = function () {
+  console.log("snorlax");
+  video.setAttribute("src", "snorlax.mp4");
+  video.play();
+  choiceDiv.style.display = "none";
+};
